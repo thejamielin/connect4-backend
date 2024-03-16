ce29c8a1aed05bebcfad40ac09712e1d389f09a3
jaroncui <jaron.cui@gmail.com>
2024-03-16 17:12:46 -0400

    started types.ts

---
+++ types.ts
@@ -0,0 +1,15 @@
+
+export interface PublicUser {
+  username: string;
+  following: string[];
+  stats: UserStats;
+}
+
+export interface PrivateUser extends PublicUser {
+  email: string;
+  admin: boolean;
+}
+
+export interface UserStats {
+  // TODO: Add stat fields
+}
